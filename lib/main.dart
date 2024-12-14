4125befe8822736939f877a601d0c1b17ef3609e
Anandito Indrasmara <115442574+Aeroskip1810@users.noreply.github.com>
2024-12-14 11:09:01 +0700

    up ulang

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:webflutter/webview_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Popoupetshop",
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const WebviewPage(),
+    );
+  }
+}
